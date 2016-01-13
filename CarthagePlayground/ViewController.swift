76ad182fc11d5b8dbd84d9f9e359cd05579758b0
ryokosuge <ryo.kosuge@gmail.com>
2016-01-13 22:57:25 +0900

    first commit.

---
+++ CarthagePlayground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CarthagePlayground
+//
+//  Created by nagisa-kosuge on 2016/01/13.
+//  Copyright © 2016年 RyoKosuge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
